af68ae48474b4da236966f7d97f3b7451f41b70c
Jason Sievert <jsievert@gmail.com>
2014-09-09 22:42:52 -0500

    Updated class to only pull once

--- thermo.php
+++ thermo.php
@@ -11,47 +11,41 @@ class Tstat
 	private $uri;
 	private $username;
 	private $password;
+	protected $HeatSet;
+	protected $CoolSet;
+	protected $IndoorTemp;
+	protected $IndoorHumidity;
+	protected $ts;
 
 	function __construct($uri, $username, $password, $SerialNumber) {
 		$this->uri = $uri;
 		$this->username = $username;
 		$this->password = $password;
 		$this->SerialNumber = $SerialNumber;
+		$this->UpdateData();
 	}
 
-	public function GetHeatSet() {
-		$systemInfo=$this->GetTStatInfo();
-		return $systemInfo["tStatInfo"]["0"]["Heat_Set_Point"];
+	public function __get($name) {
+		return isset($this->$name) ? $this->$name : null;
 	}
 
-	public function GetCoolSet() {
-		$systemInfo=$this->GetTStatInfo();
-		return $systemInfo["tStatInfo"]["0"]["Cool_Set_Point"];
-	}
-
-	public function GetIndoorTemp() {
-		$systemInfo=$this->GetTStatInfo();
-		return $systemInfo["tStatInfo"]["0"]["Indoor_Temp"];
-	}
-
-	public function GetIndoorHumidity() {
-		$systemInfo=$this->GetTStatInfo();
-		return $systemInfo["tStatInfo"]["0"]["Indoor_Humidity"];
-	}
-
-	private function GetTStatInfo() {
+	private function UpdateData() {
 		$request = $this->uri . "GetTStatInfoList?GatewaySN=" . $this->SerialNumber . "&TempUnit=&Cancel_Away=-1";
 		$r = \Httpful\Request::get($request)
 			->authenticateWith($this->username, $this->password)
 			->sendIt();
-		$systeminfo=json_decode($r, true);
-		return $systeminfo;
-
+		$systemInfo=json_decode($r, true);
+		$this->HeatSet = $systemInfo["tStatInfo"]["0"]["Heat_Set_Point"];
+		$this->CoolSet = $systemInfo["tStatInfo"]["0"]["Cool_Set_Point"];
+		$this->IndoorTemp = $systemInfo["tStatInfo"]["0"]["Indoor_Temp"];
+		$this->IndoorHumidity = $systemInfo["tStatInfo"]["0"]["Indoor_Humidity"];
+		$this->ts = substr($systemInfo["tStatInfo"]["0"]["DateTime_Mark"], 6,10);
+		$this->ts = new DateTime("@$this->ts");
 	}		
 }
 
 $home = new Tstat($uri, $username, $password, $serialnumber);
 
-echo $home->GetHeatSet();
+echo $home->ts->format('Y-m-d H:i:s');
 
 ?>
